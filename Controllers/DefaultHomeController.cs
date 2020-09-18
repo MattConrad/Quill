005fa3457d3902d8c960a4ad74a04857ef2445d0
Matt Conrad <mattconrad@gmail.com>
2020-09-18 15:14:29 -0500

    Restart Quill over from a base of ASP.NET Core 3.1. Many issues yet to work through.

---
+++ Controllers/DefaultHomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quill.Models;
+
+namespace Quill.Controllers
+{
+    public class DefaultHomeController : Controller
+    {
+        private readonly ILogger<DefaultHomeController> _logger;
+
+        public DefaultHomeController(ILogger<DefaultHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return null;
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/InkModels.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ink.Runtime;
+using Newtonsoft.Json;
+
+namespace Quill.Models
+{
+    public static class InkOutputMessageTypes
+    {
+        public static string Text = "text";
+        public static string Choice = "choice";
+        public static string Error = "error";
+    }
+
+    public class InkOutputMessage
+    {
+        //we could instead use a contract resolver, but I don't expect a lot of xfer classes. http://james.newtonking.com/archive/2013/05/08/json-net-5-0-release-5-defaultsettings-and-extension-data
+        [JsonProperty(PropertyName = "messageType")]
+        public string MessageType { get; set; }
+        [JsonProperty(PropertyName = "choiceIndex")]
+        public int ChoiceIndex { get; set; }
+        [JsonProperty(PropertyName = "outputText")]
+        public string OutputText { get; set; }
+        [JsonProperty(PropertyName = "instructions")]
+        public string[] Instructions { get; set; }
+    }
+    
+    public class CateError
+    {
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+        [JsonProperty(PropertyName = "lineNumber")]
+        public int LineNumber { get; set; }
+    }
+    
+    public static class InkMethods
+    {
+        public static List<InkOutputMessage> GetStoryOutputMessages(Story story)
+        {
+            List<InkOutputMessage> outputs = new List<InkOutputMessage>();
+            while (story.canContinue)
+            {
+                outputs.Add(new InkOutputMessage() { MessageType = InkOutputMessageTypes.Text, OutputText = story.Continue() });
+            }
+            
+            outputs.AddRange(story.currentChoices.Select(c => new InkOutputMessage() { MessageType = InkOutputMessageTypes.Choice, ChoiceIndex = c.index, OutputText = c.text }));
+            
+            outputs = outputs.Select(o => { o.OutputText = System.Net.WebUtility.HtmlEncode(o.OutputText); return o; }).ToList();
+
+            return outputs;
+        }
+        
+        public static Story LoadEmptyStory(string inkJsonPath)
+        {
+            string storyJson = System.IO.File.ReadAllText(inkJsonPath);
+
+            return new Story(storyJson);
+        }
+
+        public static Story RestoreStory(string inkJsonPath, string gameStatePath)
+        {
+            var story = LoadEmptyStory(inkJsonPath);
+
+            string storyState = System.IO.File.ReadAllText(gameStatePath);
+
+            story.state.LoadJson(storyState);
+
+            return story;
+        }
+
+        public static void SaveStory(string gameStatePath, Story story)
+        {
+            string storyState = story.state.ToJson();
+
+            //trying to get 0-turn saving to work, but no luck.
+            //storyState = storyState.Replace("\"turnIdx\":-1", "\"turnIdx\":0");
+            //storyState = storyState.Replace("\"cPath\":\"\"", "\"cXXXPath\":\"\"");
+
+            System.IO.File.WriteAllText(gameStatePath, storyState);
+        }
+        
+        
+    }
+    
+}
